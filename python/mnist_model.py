f0bb63a6fd05729fb65ab0de96fae730a06e43e6
russ-klein <russell.klein@siemens.com>
2022-05-23 15:00:41 -0700

    cleaned up code

--- python/mnist_model.py
+++ python/mnist_model.py
@@ -3,41 +3,72 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, Flatten
 from keras.utils import np_utils
+import random
 
-# load data
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+def mnist_model(num_classes):
+  # create model
+  model = Sequential()
+  model.add(Conv2D(10, (3,3), use_bias=False, padding="same", activation="relu", input_shape=(28,28,1)))
+  model.add(Conv2D(3, (3,3), use_bias=False, padding="same", activation="relu"))
+  model.add(Flatten())
+  model.add(Dense(num_classes, use_bias=False, kernel_initializer='normal', activation='softmax'))
+  # Compile model
+  model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+  return model
 
-# flatten 28*28 images to a 784 vector for each image
-num_pixels = X_train.shape[1] * X_train.shape[2]
+def create_and_train():
 
-X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], X_train.shape[2], 1)).astype('int32');
-X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], X_test.shape[2], 1)).astype('int32');
+  # load data
+  (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-# normalize inputs from 0-255 to 0-1
-X_train = X_train / 255
-X_test = X_test / 255
+  X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], X_train.shape[2], 1)).astype('int32');
+  X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], X_test.shape[2], 1)).astype('int32');
 
-# one hot encode outputs
-y_train = np_utils.to_categorical(y_train)
-y_test = np_utils.to_categorical(y_test)
+  # normalize inputs from 0-255 to 0-1
+  X_train = X_train / 255
+  X_test = X_test / 255
 
-num_classes = y_test.shape[1]
+  # one hot encode outputs
+  y_train = np_utils.to_categorical(y_train)
+  y_test = np_utils.to_categorical(y_test)
 
-# define baseline model
-def baseline_model(num_conv_out):
-	# create model
-	model = Sequential()
-	model.add(Conv2D(num_conv_out, (3,3), use_bias=False, padding="same", activation="relu", input_shape=(28,28,1)))
-	model.add(Conv2D(3, (3,3), use_bias=False, padding="same", activation="relu"))
-	model.add(Flatten())
-	model.add(Dense(num_classes, use_bias=False, kernel_initializer='normal', activation='softmax'))
-	# Compile model
-	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-	return model
+  num_classes = y_test.shape[1]
 
-n=8
-model = baseline_model(n)
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=20, batch_size=200, verbose=1)
-scores = model.evaluate(X_test, y_test, verbose=0)
-print(n, "Baseline Error: %.2f%%" % (100-scores[1]*100))
+  model = mnist_model(num_classes)
 
+  model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200, verbose=1)
+  scores = model.evaluate(X_test, y_test, verbose=0)
+  print("Baseline Error: %.2f%%" % (100-scores[1]*100))
+  return model
+
+def print_mnist_image(image):
+  local_image = image.reshape(28,28).copy()
+  for row in range(28):
+    for col in range(28):
+      print('{:3d} '.format(int(local_image[row][col]*255.0)), end='')
+    print(' ')
+  print(' ')
+
+def predict(model, image=None, label=None):
+
+  if (image == None):
+    # load data
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+    index = random.randrange(0, X_test.shape[0])
+    image = X_test[index].reshape(1, 28, 28, 1)
+    label = y_test[index]
+
+    image = image / 255.0
+
+  print_mnist_image(image)
+
+  p = model.predict(image.reshape(1,28,28,1))
+  p = p[0]
+
+  print('Label:  ', label)
+  print(' ')
+  print('Predictions: ')
+
+  for i in range(10):
+    print('p[{:d}] = {:6.4f} '.format(i, p[i]))
