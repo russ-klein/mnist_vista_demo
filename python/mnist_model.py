45a32713386346666fa685288e9569abdd7aa7ef
russ-klein <russell.klein@siemens.com>
2022-07-18 14:15:01 -0700

    added weight save/restore functions

--- python/mnist_model.py
+++ python/mnist_model.py
@@ -3,7 +3,18 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, Flatten
 from keras.utils import np_utils
+import numpy as np
 import random
+
+#===============================================================================#
+# MNIST CNN definition                                                          #
+# --------------------                                                          #
+#                                                                               #
+# Can be modified to increase or decrease number of layer, number of channels   #
+# supported layers are Conv2D, Dense, and Flatten.  Suppoted kernel sizes are   #
+# 3, 5, and 7                                                                   #
+#                                                                               #
+#===============================================================================#
 
 def mnist_model(num_classes):
   # create model
@@ -16,7 +27,18 @@ def mnist_model(num_classes):
   model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
   return model
 
-def create_and_train():
+
+#===============================================================================#
+# Create and train the MNIST CNN                                                #
+# ------------------------------                                                #
+#                                                                               #
+# Default epochs is 10, batch_size is 200.  Increase epochs and batch size for  #
+# better inferencing, 20 to 30 epochs is good for generating final weights      #
+#                                                                               #
+#===============================================================================#
+
+
+def create_and_train(epochs=10, batch_size=200, verbose=1):
 
   # load data
   (X_train, y_train), (X_test, y_test) = mnist.load_data()
@@ -36,10 +58,12 @@ def create_and_train():
 
   model = mnist_model(num_classes)
 
-  model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200, verbose=1)
+  model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=batch_size, verbose=verbose)
   scores = model.evaluate(X_test, y_test, verbose=0)
   print("Baseline Error: %.2f%%" % (100-scores[1]*100))
   return model
+
+
 
 def print_mnist_image(image):
   local_image = image.reshape(28,28).copy()
@@ -48,6 +72,8 @@ def print_mnist_image(image):
       print('{:3d} '.format(int(local_image[row][col]*255.0)), end='')
     print(' ')
   print(' ')
+
+
 
 def predict(model, image=None, label=None):
 
@@ -72,3 +98,41 @@ def predict(model, image=None, label=None):
 
   for i in range(10):
     print('p[{:d}] = {:6.4f} '.format(i, p[i]))
+
+def check_model_accuracy(model, samples = 4000):
+
+  # load data
+  (X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+  X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], X_train.shape[2], 1)).astype('int32');
+  X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], X_test.shape[2], 1)).astype('int32');
+
+  # normalize inputs from 0-255 to 0-1
+  X_train = X_train / 255
+  X_test = X_test / 255
+
+  # pick samples from test data
+  test_sample_indicies = random.sample(range(0, len(y_test)), k=samples)
+
+  X_check = X_test[test_sample_indicies];
+  y_check = y_test[test_sample_indicies];
+
+  correct_predictions = 0
+
+  for i in range(samples):
+    features = X_check[i].reshape(1, 28, 28, 1)
+    label = y_check[i]
+    if label == np.argmax(model.predict(features)):
+      correct_predictions += 1
+
+  return correct_predictions/samples      
+  
+
+def save_weights(model, weight_name="mnist_weights"):
+  model.save_weights(weight_name)
+
+
+def load_weights(model, weight_name="mnist_weights"):
+  model.load_weights(weight_name)
+
+
